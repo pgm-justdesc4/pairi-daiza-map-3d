7694423f93e2760d63c73d45c08075c603d13ebf
pgm-justdesc4 <justin.descan@student.arteveldehs.be>
2025-12-24 01:14:25 +0100

    temple model

--- src/Components/Temple/Temple.tsx
+++ src/Components/Temple/Temple.tsx
@@ -1,7 +1,7 @@
 import { useGLTF } from "@react-three/drei";
 
 export default function Temple() {
-  const { scene } = useGLTF("/Models/test.glb");
+  const { scene } = useGLTF("/Models/Temple.glb");
 
   return <primitive object={scene} />;
 }
