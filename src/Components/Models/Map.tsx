11304a0ba05fcaa74bc2800e19d802961e44ba8d
pgm-justdesc4 <justin.descan@student.arteveldehs.be>
2026-01-05 21:57:03 +0100

    removed leva toolings

--- src/Components/Models/Map.tsx
+++ src/Components/Models/Map.tsx
@@ -1,5 +1,4 @@
 import { useGLTF } from "@react-three/drei";
-import { useControls } from "leva";
 import { useEffect, useRef } from "react";
 import { RigidBody, RapierRigidBody } from "@react-three/rapier";
 import { useLoadingStore } from "../../Store/LoadingStore";
@@ -27,30 +26,13 @@ export default function Map() {
     return () => clearTimeout(timer);
   }, [setMapReady]);
 
-  const { position, rotation, scale } = useControls("Map", {
-    position: {
-      value: { x: -350.0, y: 0, z: 420.0 },
-      step: 0.1,
-    },
-    rotation: {
-      value: { x: -0.45, y: 9.2, z: 0 },
-      step: 0.01,
-    },
-    scale: {
-      value: 0.8,
-      min: 0.1,
-      max: 10,
-      step: 0.1,
-    },
-  });
-
   return (
     <RigidBody ref={rigidBodyRef} type="fixed" colliders="trimesh">
       <primitive
         object={scene}
-        position={[position.x, position.y, position.z]}
-        rotation={[rotation.x, rotation.y, rotation.z]}
-        scale={scale}
+        position={[-350.0, 0, 420.0]}
+        rotation={[-0.45, 9.2, 0]}
+        scale={0.8}
       />
     </RigidBody>
   );
