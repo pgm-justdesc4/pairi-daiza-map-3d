30c7a105af8e588cc4575989bc9a3d4818f04ac1
pgm-justdesc4 <justin.descan@student.arteveldehs.be>
2026-01-13 21:53:48 +0100

    finall

--- src/Hooks/useCameraAnimation.ts
+++ src/Hooks/useCameraAnimation.ts
@@ -1,56 +1,52 @@
 import { useThree } from "@react-three/fiber";
-import { useCallback } from "react";
 import gsap from "gsap";
 import * as THREE from "three";
 
 export function useCameraAnimation() {
   const { camera } = useThree();
 
-  const animateTo = useCallback(
-    (
-      position: [number, number, number],
-      target: [number, number, number],
-      duration: number = 2
-    ) => {
-      gsap.killTweensOf(camera.position);
+  const animateTo = (
+    position: [number, number, number],
+    target: [number, number, number],
+    duration: number = 2
+  ) => {
+    gsap.killTweensOf(camera.position);
 
-      const startPosition = camera.position.clone();
-      const startQuaternion = camera.quaternion.clone();
+    const startPosition = camera.position.clone();
+    const startQuaternion = camera.quaternion.clone();
 
-      const tempCamera = camera.clone();
-      tempCamera.position.set(position[0], position[1], position[2]);
-      tempCamera.lookAt(target[0], target[1], target[2]);
-      const endQuaternion = tempCamera.quaternion.clone();
+    const tempCamera = camera.clone();
+    tempCamera.position.set(position[0], position[1], position[2]);
+    tempCamera.lookAt(target[0], target[1], target[2]);
+    const endQuaternion = tempCamera.quaternion.clone();
 
-      const endPosition = new THREE.Vector3(
-        position[0],
-        position[1],
-        position[2]
-      );
+    const endPosition = new THREE.Vector3(
+      position[0],
+      position[1],
+      position[2]
+    );
 
-      const animationState = { t: 0 };
+    const animationState = { t: 0 };
 
-      gsap.to(animationState, {
-        t: 1,
-        duration,
-        ease: "power3.inOut",
-        onUpdate: () => {
-          camera.position.lerpVectors(
-            startPosition,
-            endPosition,
-            animationState.t
-          );
+    gsap.to(animationState, {
+      t: 1,
+      duration,
+      ease: "power3.inOut",
+      onUpdate: () => {
+        camera.position.lerpVectors(
+          startPosition,
+          endPosition,
+          animationState.t
+        );
 
-          camera.quaternion.slerpQuaternions(
-            startQuaternion,
-            endQuaternion,
-            animationState.t
-          );
-        },
-      });
-    },
-    [camera]
-  );
+        camera.quaternion.slerpQuaternions(
+          startQuaternion,
+          endQuaternion,
+          animationState.t
+        );
+      },
+    });
+  };
 
   return { animateTo };
 }
